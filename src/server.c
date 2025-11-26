44b5a4a36655fbebf6db57c2f04995f6a83d756c
202011658-eng <202011658@jbnu.ac.kr>
2025-11-26 11:22:08 +0900

    Fix buffer length check for UPDATE

--- src/server.c
+++ src/server.c
@@ -721,7 +721,7 @@ void handleClient(int client_sock, char *client_ip) {
         else if (strncmp(buffer, "ONLINE", 6) == 0) {
             listOnlineUsers(client_sock);
         }
-        else if (strncmp(buffer, "UPDATE:", 6) == 0) {
+        else if (strncmp(buffer, "UPDATE:", 7) == 0) {
             int post_id = atoi(buffer + 7);
             updatePost(client_sock, post_id, nickname);
         }
